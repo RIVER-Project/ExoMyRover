136ead445a62d588b4aa8ce3a5e142faececb163
mateisorodoc <Sorodoc.A.Matei@student.utcluj.ro>
2024-05-10 02:21:52 +0300

    am modificat camera_simulation.py dupa capul nostru
    Signed-off-by: mateisorodoc <Sorodoc.A.Matei@student.utcluj.ro>

--- Flask/camera_simulation.py
+++ Flask/camera_simulation.py
@@ -28,5 +28,5 @@ def video_feed():
 
 if __name__ == '__main__':
     time.sleep(5)
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
 
