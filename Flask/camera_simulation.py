41f757f5f17711728770859616c113ce00929660
mateisorodoc <Sorodoc.A.Matei@student.utcluj.ro>
2024-05-10 01:54:11 +0300

    am modificat camera_simulation.py dupa capul nostru
    Signed-off-by: mateisorodoc <Sorodoc.A.Matei@student.utcluj.ro>

--- Flask/camera_simulation.py
+++ Flask/camera_simulation.py
@@ -1,17 +1,17 @@
-from picamera2 import Picamera2
 import picamera2
+from picamera2 import Picamera2
 import io
 from flask import Flask, Response, render_template
+import threading
 
 app = Flask(__name__)
-
 
 # Create a streaming output object
 class StreamingOutput(object):
     def __init__(self):
         self.frame = None
         self.buffer = io.BytesIO()
-        self.condition = picamera2.Picamera2Lock()
+        self.condition = threading.Condition()
 
     def write(self, buf):
         if buf.startswith(b'\xff\xd8'):
@@ -23,18 +23,15 @@ class StreamingOutput(object):
             self.buffer.seek(0)
         return self.buffer.write(buf)
 
-
 # Camera initialization
 camera = Picamera2()
 camera.resolution = (640, 480)
 output = StreamingOutput()
 camera.start_recording(output, format='mjpeg')
 
-
 @app.route('/')
 def index():
     return render_template('camera.html')
-
 
 # Generator function for streaming frames
 def generate():
@@ -45,12 +42,10 @@ def generate():
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-
 # Route for video feed
 @app.route('/video_feed')
 def video_feed():
     return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
