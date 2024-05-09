c0f69b889acdc9708b167471613bdb720275fc4b
mateisorodoc <Sorodoc.A.Matei@student.utcluj.ro>
2024-05-09 20:12:08 +0300

    Adaugat stop endpoint si functiile din RoverOOP pentru move forward/backward pe drept si stop
    Signed-off-by: mateisorodoc <Sorodoc.A.Matei@student.utcluj.ro>

--- Flask/app.py
+++ Flask/app.py
@@ -1,26 +1,53 @@
 from flask import Flask, render_template, request
 
+from RoverOOP import Rover, Joint
+
 app = Flask(__name__)
+
+FLJ = Joint(0, 6)  # Front Left Joint
+FRJ = Joint(1, 7)  # Front Right Joint
+MLJ = Joint(2, 8)  # Middle Left Joint
+MRJ = Joint(3, 9)  # Middle Right Joint
+RLJ = Joint(4, 10)  # Rear Left Joint
+RRJ = Joint(5, 11)  # Rear Right Joint
+
+# Create the Rover object, having as arguments the 6 Joint objects
+Rover_obj = Rover(FLJ, FRJ, MLJ, MRJ, RLJ, RRJ)
+
 
 def move_forward():
     # Code to move the robot forward
-    return 'Moving forward'
+    Rover_obj.Move_forward(90)
+
 
 def move_backward():
     # Code to move the robot backward
-    return 'Moving backward'
+    Rover_obj.Move_backward(90)
+
 
 def move_left():
     # Code to move the robot left
     return 'Moving left'
 
+
 def move_right():
     # Code to move the robot right
     return 'Moving right'
 
+
+def stop_rover():
+    Rover_obj.Stop_rover()
+
+
 @app.route('/')
 def index():
     return render_template('index1.html')
+
+
+@app.route('/stop', methods=['POST'])
+def stop():
+    return stop_rover()
+
 
 # Move endpoint
 @app.route('/move', methods=['POST'])
@@ -37,5 +64,6 @@ def move():
     else:
         return 'Invalid direction'
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run('0.0.0.0', 5420, debug=True)
--- Flask/camera_simulation.py
+++ Flask/camera_simulation.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 camera = cv2.VideoCapture(0)  # Initialize camera
 
+
 def gen_frames():
     while True:
         success, frame = camera.read()  # Read frame from camera
@@ -16,9 +17,11 @@ def gen_frames():
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/video_feed')
 def video_feed():
@@ -26,4 +29,6 @@ def video_feed():
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0',port=5001 ,debug=True)
+
+
+    app.run('0.0.0.0', port=5069, debug=True)
