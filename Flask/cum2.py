48bd910387ca9b4398dd1c865aa8627832112c97
mateisorodoc <Sorodoc.A.Matei@student.utcluj.ro>
2024-05-10 01:39:47 +0300

    am modificat camera_simulation.py dupa capul nostru
    Signed-off-by: mateisorodoc <Sorodoc.A.Matei@student.utcluj.ro>

--- Flask/cum2.py
+++ Flask/cum2.py
@@ -1,6 +1,4 @@
 import picamera2  # camera module for RPi camera
-from picamera2 import Picamera2
-from picamera2.encoders import JpegEncoder, H264Encoder
 import io
 
 from flask import Flask, Response, request, render_template
@@ -23,17 +21,19 @@ class StreamingOutput(io.BufferedIOBase):
 
 
 # defines the function that generates our frames
+# defines the function that generates our frames
 def genFrames():
-    with picamera2.Picamera2() as camera:
-        camera.configure(camera.create_video_configuration(main={"size": (640, 480)}))
-        output3 = StreamingOutput()
-        camera.start()
-        while True:
-            with output3.condition:
-                output3.condition.wait()
-            frame = output3.frame
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    camera = picamera2.Picamera2()
+    camera.configure(camera.create_video_configuration(main={"size": (640, 480)}))
+    output3 = StreamingOutput()
+    camera.start()
+    while True:
+        with output3.condition:
+            output3.condition.wait()
+        frame = output3.frame
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 
 
 # defines the route that will access the video feed and call the feed function
