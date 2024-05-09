ecadb0c80afed7217628d122fae665fc18939e6d
mateisorodoc <Sorodoc.A.Matei@student.utcluj.ro>
2024-05-10 02:07:47 +0300

    am modificat camera_simulation.py dupa capul nostru
    Signed-off-by: mateisorodoc <Sorodoc.A.Matei@student.utcluj.ro>

--- Flask/camera_simulation.py
+++ Flask/camera_simulation.py
@@ -1,16 +1,19 @@
 from picamera2 import Picamera2
 from flask import Flask, Response
+import cv2
 
 app = Flask(__name__)
 
 # Initialize the camera
 camera = Picamera2()
 camera.resolution = (640, 480)
+camera.start()
 
 # Generator function for streaming frames
 def generate_frames():
     while True:
         frame = camera.capture_array(format='jpeg', use_video_port=True)
+        print(frame)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame.tobytes() + b'\r\n')
 
@@ -21,3 +24,4 @@ def video_feed():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+
